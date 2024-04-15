e654ed524ebcc8696638fb1f49fe2481db1db971
AlexanderFaezov <s.faezov.com@mail.ru>
2024-04-15 17:31:03 +0500

    Добавьте файлы проекта.

---
+++ Sem2_OOP5.cpp
@@ -0,0 +1,21 @@
+﻿#include "Vector.h"
+#include "Student.h"
+
+int main()
+{
+    system("chcp 1251");
+
+    int Length;
+
+    do
+    {
+        cout << "Введите размер вектора" << endl;
+        cin >> Length;
+    } while (Length <= 0);
+
+    Vector vector(Length);
+
+    cin >> vector;
+
+    cout << vector << endl;
+}
